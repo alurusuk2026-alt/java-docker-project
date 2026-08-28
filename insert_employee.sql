2345d26e464d602efdf6f95b0feb99a1c3a316d3
reshmas <alurusuk2026@email.com>
2026-08-28 17:40:29 +0530

    Add production deployment pipeline with approvals

--- insert_employee.sql
+++ insert_employee.sql
@@ -1,2 +1,2 @@
 INSERT INTO `employee` VALUES (6,'QA','ravi@example.com','Ravi');
-INSERT INTO `employee` VALUES (7,'dev','reshma@example.com','reshma');
+INSERT INTO `employee` VALUES (9,'dev','reshma@example.com','reshma');
