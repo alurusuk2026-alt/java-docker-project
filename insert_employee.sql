ad92dc665a8ee9ad0da0b9d2356a7433abe433d9
reshmas <alurusuk2026@email.com>
2026-08-28 15:42:24 +0530

    added file

--- insert_employee.sql
+++ insert_employee.sql
@@ -1,2 +1,2 @@
-INSERT INTO employee (department, email, name)
-VALUES ('Development', 'anita@example.com', 'Anita');
+INSERT INTO `employee` VALUES (2,'QA','ravi@example.com','Ravi');
+INSERT INTO `employee` VALUES (3,'dev','reshma@example.com','reshma');
