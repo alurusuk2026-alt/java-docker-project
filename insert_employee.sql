76052fe8422d869272f2b424a3619d87a5f814e3
reshmas <alurusuk2026@email.com>
2026-08-28 16:13:52 +0530

    removed data file

--- insert_employee.sql
+++ insert_employee.sql
@@ -1,2 +1,2 @@
-INSERT INTO `employee` VALUES (2,'QA','ravi@example.com','Ravi');
-INSERT INTO `employee` VALUES (3,'dev','reshma@example.com','reshma');
+INSERT INTO `employee` VALUES (3,'QA','ravi@example.com','Ravi');
+INSERT INTO `employee` VALUES (4,'dev','reshma@example.com','reshma');
